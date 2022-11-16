1c0deccd011bba6463b144f641425742709fdf7f
William Zinkann <wzinkann@gmail.com>
2022-11-16 15:39:49 -0500

    added pause method

--- src/controller/WatorController.java
+++ src/controller/WatorController.java
@@ -14,17 +14,21 @@ public class WatorController {
 	private WatorModel model;
 	private WatorView simulationDisplay;
 
-	public WatorController(int width, int height, WatorView simulationDisplay) {	
-			this.simulationDisplay=  simulationDisplay;
-			this.model = new WatorModel(width,height);
-		}
-	
+	public WatorController(int width, int height, WatorView simulationDisplay) {
+		this.simulationDisplay = simulationDisplay;
+		this.model = new WatorModel(width, height);
+	}
+
 	public void newSimulation() {
 		model.createSimulation();
 		simulationDisplay.redraw();
 	}
 	
-	public void doOneStep(double elapsedTime){
+	public void pauseSimulation() {
+
+	}
+
+	public void doOneStep(double elapsedTime) {
 		model.step();
 		simulationDisplay.redraw();
 	}
@@ -32,6 +36,5 @@ public class WatorController {
 	public int getCellState(Point position) {
 		return model.get(position);
 	}
-	
-	
+
 }
