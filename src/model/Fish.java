f497bb7258c02621fe49bfb7418f9e4c13b9dd40
TrevorRau <trau@elon.edu>
2022-11-23 13:05:02 -0500

    add rectangle and color

--- src/model/Fish.java
+++ src/model/Fish.java
@@ -1,10 +1,13 @@
 package model;
 
+import javafx.scene.paint.Color;
+
 public class Fish extends WatorCreature{
 
 	public Fish(int tag, int row, int column, WatorObject[][] watorWorld) {
 		super(tag, row, column, watorWorld);
 		super.myBreedTime = 1;
+		myRectangle.setFill(Color.GREEN);
 	}
 	
 	
